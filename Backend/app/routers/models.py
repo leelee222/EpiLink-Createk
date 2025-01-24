21922e4e1d55a52ce1cebfb95a6e5fe6de8930a8
AnalyticAce <shalom.dosseh@epitech.eu>
2025-01-24 23:09:39 +0100

    create update model

--- Backend/app/routers/models.py
+++ Backend/app/routers/models.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, EmailStr, Field
-from typing import Optional
+from datetime import datetime
+from typing import List, Optional
 
 class Token(BaseModel):
     access_token: str
@@ -13,6 +14,9 @@ class User(BaseModel):
     email: Optional[EmailStr] = Field(None, example="johndoe@gmail.com")
     disabled: Optional[bool] = False
     provider: Optional[str] = "createk"
+    following: Optional[list[str]] = []
+    followers: Optional[list[str]] = []
+    social_links: Optional[dict[str, str]] = {}
 
 class UserInDB(User):
     hashed_password: Optional[str] = None
@@ -21,3 +25,39 @@ class UserCreate(BaseModel):
     username: str
     email: str
     password: str
+
+class PostCreate(BaseModel):
+    title: str
+    content: str
+
+class Post(PostCreate):
+    id: str
+    author_id: str
+    likes: List[str] = []
+    comments: List['Comment'] = []
+    created_at: datetime
+    updated_at: datetime
+
+class CommentCreate(BaseModel):
+    content: str
+
+class Comment(CommentCreate):
+    id: str
+    author_id: str
+    likes: List[str] = []
+    replies: List['Reply'] = []
+    created_at: datetime
+    updated_at: datetime
+
+class ReplyCreate(BaseModel):
+    content: str
+
+class Reply(ReplyCreate):
+    id: str
+    author_id: str
+    likes: List[str] = []
+    created_at: datetime
+    updated_at: datetime
+
+Post.update_forward_refs()
+Comment.update_forward_refs()
