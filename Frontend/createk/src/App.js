eea92ee5f8452a487adba369ebcf6ea04f869b70
joel alawenon first submit <joel.alawenon@epitech.eu>
2025-01-27 13:02:10 +0100

    [Update] project name

--- Frontend/createk/src/App.js
+++ Frontend/createk/src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useContext, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+
 
 function App() {
   return (
