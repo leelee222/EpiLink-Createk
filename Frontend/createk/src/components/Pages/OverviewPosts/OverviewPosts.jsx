aeb6493871b17bf7c7463afd50af80bb0f3f4326
joel alawenon first submit <joel.alawenon@epitech.eu>
2025-01-27 13:38:22 +0100

    [Add] overview posts jsx

---
+++ Frontend/createk/src/components/Pages/OverviewPosts/OverviewPosts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './OverviewPosts.css';
+
+const OverviewPosts = () => {
+  return (
+    <div>
+        hello overview
+    </div>
+  )
+}
+
+export default OverviewPosts;
