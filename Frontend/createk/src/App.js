986edff97911c03d2c30ecffd0d1f1a37ee6a4c6
ilham <ilham.adios@epitech.eu>
2025-01-28 19:44:44 +0100

    adding routes to home, profile and login and protected routes

--- Frontend/createk/src/App.js
+++ Frontend/createk/src/App.js
@@ -1,18 +1,41 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './components/Authentification/Login';
+import Success from './components/Authentification/Sucess';
+import HomePage from './components/Pages/Explore';
 import OverviewPosts from './components/Pages/OverviewPosts/OverviewPosts';
+import ProfilePage from './components/Pages/Profile';
+
+const Check = () => {
+  const ProtectedRoute = ({ children }) => {
+    const accessToken = localStorage.getItem('access_token');
+    console.log('Access Token:', accessToken);
+    return accessToken ? (
+      <>
+        {children}
+      </>
+    ) : (
+      <Navigate to="/login" replace />
+    );  };
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route exact path='/login' element={<LoginPage />} />
+        <Route path="/success" element={<Success />} />
+        <Route exact path='/' element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
+        <Route exact path='/overviews' element={<ProtectedRoute><OverviewPosts /></ProtectedRoute>} />
+        <Route exact path='/profile' element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
+      </Routes>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route exact path='/'/>
-          <Route exact path="/overview" element={<OverviewPosts />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Check />
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
